1a82f2b1c8fd7563960fc1b4f5179b82fe7641ba
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-07-16 13:36:06 -0500

    Product model

---
+++ admin-dashboard/frontend/src/models/Product.ts
@@ -0,0 +1,11 @@
+export default interface product {
+  product_id: number;
+  name: string;
+  image_url: string;
+  description: string;
+  category: string;
+  quantity: number;
+  price: number;
+  date_created: number;
+  date_modified: number;
+}
