1e4da70ea7f55d157d4a209785f46c79c1df47bf
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-07-16 13:38:14 -0500

    Import cleanup

--- admin-dashboard/backend/admin/src/main/java/com/ecommerce/admin/ProductService.java
+++ admin-dashboard/backend/admin/src/main/java/com/ecommerce/admin/ProductService.java
@@ -1,20 +1,16 @@
 package com.ecommerce.admin;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.admin.lib.ProductRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -30,7 +26,7 @@ public class ProductService {
     @RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public Mono<String> findProducts() {
         return productRepository.findProducts()
-                .collectList() // collect flux into list
+                .collectList()
                 .map(products -> {
                     try {
                         return new ObjectMapper().writeValueAsString(products);
